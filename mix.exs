3b3430e72d76cea8a94c4f203597f004bdcce5d1
Rafael Jesus <rafaelljesus86@gmail.com>
2016-06-05 21:22:38 -0300

    First commit

---
+++ mix.exs
@@ -0,0 +1,36 @@
+defmodule Kv.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :kv,
+     version: "0.0.1",
+     elixir: "~> 1.2",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    [applications: [:logger, :postgrex, :ecto],
+      mod: {Kv, []}]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:mydep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
+  defp deps do
+    [
+      {:postgrex, "~> 0.11.1"},
+      {:ecto, "~> 2.0.0-rc.5"}
+    ]
+  end
+end
---
+++ test/kv_test.exs
@@ -0,0 +1,8 @@
+defmodule KvTest do
+  use ExUnit.Case
+  doctest Kv
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
