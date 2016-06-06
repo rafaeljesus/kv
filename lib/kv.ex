7c3c9852a84e27f7cf374b344fa131706ba721a6
Rafael Jesus <rafaelljesus86@gmail.com>
2016-06-05 21:29:24 -0300

    Implement Supervisor

--- lib/kv.ex
+++ lib/kv.ex
@@ -1,2 +1,17 @@
 defmodule Kv do
+  use Application
+  import Supervisor.Spec, warn: false
+
+  def start(_type, _args) do
+    children = [
+      supervisor(Kv.Repo, []),
+      worker(Kv.Worker, [])
+    ]
+    options = [strategy: :one_for_one, name: Kv.Supervisor]
+    Supervisor.start_link(children, options)
+  end
+
+  defdelegate set(k, v), to: Kv.Worker
+  defdelegate get(k), to: Kv.Worker
+  defdelegate clear, to: Kv.Worker
 end
